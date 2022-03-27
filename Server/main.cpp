75f31845d50c7162239a380726b9ed9e86e0bc31
Qzarov <qzarov@mail.ru>
2022-03-27 21:27:09 +0300

    Server's prototype ready

--- Server/main.cpp
+++ Server/main.cpp
@@ -25,13 +25,13 @@ int main()
     inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr); // 127.0.0.1
 
     if (bind(listening, (sockaddr*)&hint, sizeof(hint)) == -1) {
-        std::cerr << "Can't bind to IP/port";
+        std::cerr << "Can't bind to IP/port" << std::endl;
         return -2;
     }
 
     // Mark the socket for listening in
     if (listen(listening, SOMAXCONN) == -1) {
-        std::cerr << "Can't listen!";
+        std::cerr << "Can't listen!" << std::endl;
         return -3;
     }
 
@@ -44,10 +44,11 @@ int main()
     int client_socket = accept(listening, (sockaddr*)&hint, &client_size);
 
     if (client_socket == -1) {
-        std::cerr << "Problem with clients connecting";
+        std::cerr << "Problem with clients connecting" << std::endl;
         return -4;
     }
 
+    // Close the listening socket
     close(listening);
     memset(host, 0, NI_MAXHOST);
     memset(svc, 0, NI_MAXSERV);
@@ -60,18 +61,35 @@ int main()
         inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
         std::cout << host << " connected on " << ntohs(client.sin_port) << std::endl;
     }
-
-    // Close the listening socket
     
-    // While loop, echo message
+    // Wait for a message
     char buf[4096];
-    while (true) {
+    std::string inp;
+    while (inp != "SIGTERM" and inp != "SIGHUP") {
         // Clear the buffer
-        //
+        memset(buf, 0, 4096);
+        int bytes_recv = recv(client_socket, buf, 4096, 0);
+        
+        if (bytes_recv == 0) {
+            std::cerr << "The client disconnected" << std::endl;
+            break;
+        }
+        
+        if (bytes_recv == -1) {
+            std::cerr << "There was a connection issue" << std::endl;
+            break;
+        }
+
+        // Display message
+        inp = std::string(buf, 0, bytes_recv);
+        std::cout << "Received: " << inp << std::endl;
+
+        // Redend message
+        send(client_socket, buf, bytes_recv + 1, 0);        
     }
 
-
     // Close socket
-    
+    close(client_socket);
+
     return 0;
 }
